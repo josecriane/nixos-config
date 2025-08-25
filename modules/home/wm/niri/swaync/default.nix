52e17482d4eb660eab76b5fc58f545ccddd47837
Jose E. Cribeiro Aneiros <josecriane@gmail.com>
2025-08-25 22:27:26 +0200

    feat: Adds style to waybar and swaync

--- modules/home/wm/niri/swaync/default.nix
+++ modules/home/wm/niri/swaync/default.nix
@@ -1,64 +1,15 @@
 { pkgs, config, ... }:
+let
+  stylixCss = import ../niri-utils/stylix-css.nix { inherit config; };
+in
 {
   home.packages = with pkgs; [
     swaynotificationcenter
   ];
 
-  xdg.configFile."swaync/style.css".text =
-    let
-      colors = config.lib.stylix.colors.withHashtag;
-      # Valores RGB de los colores Nord para usar con rgba()
-      rgba = {
-        base00 = "46, 52, 64"; # 2e3440
-        base01 = "59, 66, 82"; # 3b4252
-        base02 = "67, 76, 94"; # 434c5e
-        base03 = "76, 86, 106"; # 4c566a
-        base04 = "216, 222, 233"; # d8dee9
-        base05 = "229, 233, 240"; # e5e9f0
-        base06 = "236, 239, 244"; # eceff4
-        base08 = "191, 97, 106"; # bf616a - rojo
-        base0A = "235, 203, 139"; # ebcb8b - amarillo
-        base0B = "163, 190, 140"; # a3be8c - verde
-      };
-    in
-    builtins.replaceStrings
-      [
-        "@base00@"
-        "@base01@"
-        "@base02@"
-        "@base03@"
-        "@base04@"
-        "@base05@"
-        "@base06@"
-        "@base08@"
-        "@base0A@"
-        "@base0B@"
-        "@base00-rgba@"
-        "@base01-rgba@"
-        "@base02-rgba@"
-        "@base04-rgba@"
-        "@base05-rgba@"
-        "@base08-rgba@"
-      ]
-      [
-        colors.base00
-        colors.base01
-        colors.base02
-        colors.base03
-        colors.base04
-        colors.base05
-        colors.base06
-        colors.base08
-        colors.base0A
-        colors.base0B
-        rgba.base00
-        rgba.base01
-        rgba.base02
-        rgba.base04
-        rgba.base05
-        rgba.base08
-      ]
-      (builtins.readFile ./style.css);
+  xdg.configFile."swaync/style.css".text = stylixCss.replaceStylixColors (
+    builtins.readFile ./style.css
+  );
 
   xdg.configFile."swaync/config.json".source = ./config.json;
 }
--- modules/home/wm/niri/waybar/default.nix
+++ modules/home/wm/niri/waybar/default.nix
@@ -1,76 +1,11 @@
 { pkgs, config, ... }:
+let
+  stylixCss = import ../niri-utils/stylix-css.nix { inherit config; };
+in
 {
   programs.waybar = {
     enable = true;
-    style =
-      let
-        colors = config.lib.stylix.colors.withHashtag;
-        # Valores RGB de los colores Nord para usar con rgba()
-        rgba = {
-          base00 = "46, 52, 64"; # 2e3440
-          base01 = "59, 66, 82"; # 3b4252
-          base02 = "67, 76, 94"; # 434c5e
-          base03 = "76, 86, 106"; # 4c566a
-          base04 = "216, 222, 233"; # d8dee9
-          base05 = "229, 233, 240"; # e5e9f0
-          base06 = "236, 239, 244"; # eceff4
-          base07 = "143, 188, 187"; # 8fbcbb
-          base08 = "191, 97, 106"; # bf616a
-          base09 = "208, 135, 112"; # d08770
-          base0A = "235, 203, 139"; # ebcb8b
-          base0B = "163, 190, 140"; # a3be8c
-          base0C = "136, 192, 208"; # 88c0d0
-          base0D = "129, 161, 193"; # 81a1c1
-          base0E = "180, 142, 173"; # b48ead
-          base0F = "94, 129, 172"; # 5e81ac
-        };
-      in
-      builtins.replaceStrings
-        [
-          "@base00@"
-          "@base01@"
-          "@base02@"
-          "@base03@"
-          "@base04@"
-          "@base05@"
-          "@base06@"
-          "@base07@"
-          "@base08@"
-          "@base09@"
-          "@base0A@"
-          "@base0B@"
-          "@base0C@"
-          "@base0D@"
-          "@base0E@"
-          "@base0F@"
-          "@base00-rgba@"
-          "@base01-rgba@"
-          "@base04-rgba@"
-          "@base05-rgba@"
-        ]
-        [
-          colors.base00
-          colors.base01
-          colors.base02
-          colors.base03
-          colors.base04
-          colors.base05
-          colors.base06
-          colors.base07
-          colors.base08
-          colors.base09
-          colors.base0A
-          colors.base0B
-          colors.base0C
-          colors.base0D
-          colors.base0E
-          colors.base0F
-          rgba.base00
-          rgba.base01
-          rgba.base04
-          rgba.base05
-        ]
-        (builtins.readFile ./style.css);
+    style = stylixCss.replaceStylixColors (builtins.readFile ./style.css);
 
     settings = [
       {
