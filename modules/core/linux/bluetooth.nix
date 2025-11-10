0750fd13faf8e57ce7c2f51c2e1edecaa0541c0e
Jose E. Cribeiro Aneiros <josecriane@gmail.com>
2025-11-10 15:45:24 +0100

    feat: Doc STIG recommendations

--- modules/core/linux/bluetooth.nix
+++ modules/core/linux/bluetooth.nix
@@ -1,5 +1,9 @@
 { pkgs, ... }:
 {
+  # STIG V-268147: Disable Bluetooth adapters (INTENTIONALLY NOT FOLLOWED)
+  # https://stigviewer.com/stigs/anduril_nixos/2024-10-25/finding/V-268147
+  # NOTE: Bluetooth is required for operational needs and is consciously enabled despite STIG recommendation.
+  # Risk accepted: Bluetooth functionality is necessary for peripheral device connectivity.
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
--- modules/core/linux/networking.nix
+++ modules/core/linux/networking.nix
@@ -3,7 +3,15 @@
   networking = {
     hostName = "${machineOptions.hostname}";
     networkmanager.enable = true;
+    # STIG V-268078: Enable built-in firewall
+    # https://stigviewer.com/stigs/anduril_nixos/2024-10-25/finding/V-268078
     firewall.enable = true;
+
+    # STIG V-268146: Encrypt wireless access to/from system (INTENTIONALLY NOT FOLLOWED)
+    # https://stigviewer.com/stigs/anduril_nixos/2024-10-25/finding/V-268146
+    # NOTE: Wireless/WiFi connectivity is required for operational needs and is consciously enabled despite STIG recommendation.
+    # Risk accepted: WiFi functionality is necessary for network connectivity on mobile devices.
+    # wireless.enable = false;  # STIG recommends disabling, but we need WiFi via NetworkManager
   };
 
   systemd.services.NetworkManager-wait-online.enable = false;
