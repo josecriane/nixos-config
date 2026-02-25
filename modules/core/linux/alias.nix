ff4718852431014913172eed7f0aee3ad239ab32
Jose E. Cribeiro Aneiros <josecriane@gmail.com>
2026-02-25 16:09:21 +0100

    fix: Fixes fetch secret submodules

--- modules/core/linux/alias.nix
+++ modules/core/linux/alias.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 {
   environment.shellAliases = {
-    nixrebuild = "sudo nixos-rebuild switch --flake ~/nixos-config";
+    nixrebuild = "sudo SSH_AUTH_SOCK=$SSH_AUTH_SOCK nixos-rebuild switch --flake ~/nixos-config";
     nixupdate = "sudo nixos-rebuild switch --upgrade";
     open = "xdg-open";
   };
