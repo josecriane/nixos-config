97baa8eb9bddc0fa9d59e27d69c9f61abff59780
Jose E. Cribeiro Aneiros <josecriane@gmail.com>
2026-03-27 23:26:09 +0100

    feat: fix zellij stylix

--- modules/home/all/zellij.nix
+++ modules/home/all/zellij.nix
@@ -4,6 +4,23 @@
   lib,
   ...
 }:
+let
+  colors = config.lib.stylix.colors;
+
+  # Stylix provides per-channel RGB decimals: baseXX-rgb-{r,g,b}
+  c = name: "${colors."${name}-rgb-r"} ${colors."${name}-rgb-g"} ${colors."${name}-rgb-b"}";
+
+  # Generate a color group block for zellij KDL theme
+  colorGroup = name: base: bg: e0: e1: e2: e3: ''
+    ${name} {
+      base ${c base}
+      background ${c bg}
+      emphasis_0 ${c e0}
+      emphasis_1 ${c e1}
+      emphasis_2 ${c e2}
+      emphasis_3 ${c e3}
+    }'';
+in
 {
 
   programs.zellij = {
@@ -15,6 +32,7 @@
       mouse_mode = true;
       show_startup_tips = false;
       simplified_ui = false;
+      theme = "stylix";
       pane_frames = false;
       session_serialization = true;
       resurrect_dead_sessions = true;
@@ -38,7 +56,40 @@
     };
   };
 
-  stylix.targets.zellij.enable = true;
+  # Stylix's zellij target generates hex colors, but zellij 0.44+ needs RGB decimal
+  stylix.targets.zellij.enable = false;
+
+  programs.zellij.extraConfig = ''
+    themes {
+      stylix {
+    ${colorGroup "text_unselected" "base05" "base01" "base09" "base0C" "base0B" "base0F"}
+    ${colorGroup "text_selected" "base05" "base04" "base09" "base0C" "base0B" "base0F"}
+    ${colorGroup "ribbon_selected" "base01" "base0E" "base08" "base09" "base0F" "base0D"}
+    ${colorGroup "ribbon_unselected" "base01" "base05" "base08" "base05" "base0D" "base0F"}
+    ${colorGroup "table_title" "base0E" "base00" "base09" "base0C" "base0B" "base0F"}
+    ${colorGroup "table_cell_selected" "base05" "base04" "base09" "base0C" "base0B" "base0F"}
+    ${colorGroup "table_cell_unselected" "base05" "base01" "base09" "base0C" "base0B" "base0F"}
+    ${colorGroup "list_selected" "base05" "base04" "base09" "base0C" "base0B" "base0F"}
+    ${colorGroup "list_unselected" "base05" "base01" "base09" "base0C" "base0B" "base0F"}
+    ${colorGroup "frame_selected" "base0E" "base00" "base09" "base0C" "base0F" "base00"}
+    ${colorGroup "frame_highlight" "base08" "base00" "base0F" "base09" "base09" "base09"}
+    ${colorGroup "exit_code_success" "base0B" "base00" "base0C" "base01" "base0F" "base0D"}
+    ${colorGroup "exit_code_error" "base08" "base00" "base0A" "base00" "base00" "base00"}
+        multiplayer_user_colors {
+          player_1 ${c "base0F"}
+          player_2 ${c "base0D"}
+          player_3 ${c "base00"}
+          player_4 ${c "base0A"}
+          player_5 ${c "base0C"}
+          player_6 ${c "base00"}
+          player_7 ${c "base08"}
+          player_8 ${c "base00"}
+          player_9 ${c "base00"}
+          player_10 ${c "base00"}
+        }
+      }
+    }
+  '';
 
   home.shellAliases = {
     zj = "zellij";
